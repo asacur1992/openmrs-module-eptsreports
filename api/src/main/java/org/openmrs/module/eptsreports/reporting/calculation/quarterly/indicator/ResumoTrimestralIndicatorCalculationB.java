e24e5642851dd2d00deec5f23493dc250e838f27
guimino <minoneves@gmail.com>
2020-03-12 15:08:03 +0200

    updates on Resumo Trimestral

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/quarterly/indicator/ResumoTrimestralIndicatorCalculationB.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/quarterly/indicator/ResumoTrimestralIndicatorCalculationB.java
@@ -2,46 +2,44 @@ package org.openmrs.module.eptsreports.reporting.calculation.quarterly.indicator
 
 import java.util.List;
 import java.util.Map;
-
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.module.eptsreports.reporting.calculation.BooleanResult;
 import org.openmrs.module.eptsreports.reporting.calculation.quarterly.MonthlyDateRange;
-import org.openmrs.module.eptsreports.reporting.calculation.quarterly.ResumoTrimestralDateRangesCalculation;
 import org.openmrs.module.eptsreports.reporting.calculation.quarterly.ResumoTrimestralMonthPeriodCalculation;
 import org.openmrs.module.eptsreports.reporting.calculation.quarterly.query.ResumoTrimestralQueries;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 
 public abstract class ResumoTrimestralIndicatorCalculationB
-extends ResumoTrimestralMonthPeriodCalculation {
+    extends ResumoTrimestralMonthPeriodCalculation {
 
-	@Override
-	public CalculationResultMap evaluate(
-			final Map<String, Object> parameterValues, final EvaluationContext context) {
+  @Override
+  public CalculationResultMap evaluate(
+      Map<String, Object> parameterValues, EvaluationContext context) {
 
-		final CalculationResultMap resultMap = new CalculationResultMap();
+    CalculationResultMap resultMap = new CalculationResultMap();
 
-		final CalculationResultMap evaluated = super.evaluate(parameterValues, context);
-		final MonthlyDateRange monthlExecutionPeriod =
-				(MonthlyDateRange) evaluated.get(ResumoTrimestralDateRangesCalculation.MONTHLY_EXCUTION_PERIOD).getValue();
+    CalculationResultMap evaluated = super.evaluate(parameterValues, context);
+    MonthlyDateRange monthlExecutionPeriod =
+        (MonthlyDateRange) evaluated.get(MONTHLY_EXCUTION_PERIOD).getValue();
 
-		if (monthlExecutionPeriod != null) {
+    if (monthlExecutionPeriod != null) {
 
-			final List<Integer> patientIds =
-					ResumoTrimestralQueries.findPatientsWhoAreNewlyEnrolledOnART(
-							context, monthlExecutionPeriod);
+      List<Integer> patientIds =
+          ResumoTrimestralQueries.findPatientsWhoAreNewlyEnrolledOnART(
+              context, monthlExecutionPeriod);
 
-			final List<Integer> inclusions =
-					ResumoTrimestralQueries.findPatientsWhoWhereMarkedAsTransferreInByMinTransferredDate(
-							context, monthlExecutionPeriod);
+      List<Integer> inclusions =
+          ResumoTrimestralQueries.findPatientsWhoWhereMarkedAsTransferreInByMinTransferredDate(
+              context, monthlExecutionPeriod);
 
-			patientIds.retainAll(inclusions);
+      patientIds.retainAll(inclusions);
 
-			patientIds.retainAll(ResumoTrimestralQueries.getBaseCohort(context, monthlExecutionPeriod));
+      patientIds.retainAll(ResumoTrimestralQueries.getBaseCohort(context, monthlExecutionPeriod));
 
-			for (final Integer patientId : patientIds) {
-				resultMap.put(patientId, new BooleanResult(Boolean.TRUE, this));
-			}
-		}
-		return resultMap;
-	}
+      for (Integer patientId : patientIds) {
+        resultMap.put(patientId, new BooleanResult(Boolean.TRUE, this));
+      }
+    }
+    return resultMap;
+  }
 }
